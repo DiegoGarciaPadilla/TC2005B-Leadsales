428f13ec82b47f744c7fa5e3b8dca556343e053b
DiegoGarciaPadilla <gpdiego370@outlook.com>
2024-04-02 18:45:13 -0600

    Documentación

---
+++ util/db/db.js
@@ -0,0 +1,7 @@
+// Se requiere crear un archivo llamado keys.json en la carpeta util/db con la siguiente estructura:
+// {
+//     "host": "localhost",
+//     "user": "",
+//     "password": "",
+//     "database": ""
+// }
