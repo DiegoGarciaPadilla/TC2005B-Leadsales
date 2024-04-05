f4828e557e3ce8390e457a27a356da9357fbd22a
DiegoGarciaPadilla <gpdiego370@outlook.com>
2024-04-05 00:12:36 -0600

    fix(roles): validate role's privileges

---
+++ util/privilegios/privilegios.js
@@ -0,0 +1,28 @@
+exports.ver_roles = (request, response, next) => {
+    // Inicializa la variable de privilegio
+    let puedeVerRoles = false;
+
+    // Obtiene los privilegios del usuario
+    const { Privilegios } = request.session;
+    console.log("Privilegios: ", Privilegios);
+
+    // Verifica si el usuario tiene el privilegio de ver roles
+    for (let i = 0; i < Privilegios.length; i++) {
+        if (Privilegios[i].Descripcion === 'Consulta rol.') {
+            puedeVerRoles = true;
+            break;
+        }
+    }
+
+    // Si el usuario tiene el privilegio de ver roles, permite continuar con la operacion
+    if (puedeVerRoles) {
+        next();
+    }
+
+    // Si el usuario no tiene el privilegio de ver roles, redirige a la pagina de inicio
+    else {
+        response.redirect('/');
+        console.log('No tiene el privilegio de ver roles');
+    }
+
+}
