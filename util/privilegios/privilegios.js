6aa8c886c0891419aae27d9e611ee0ddc64c1395
DiegoGarciaPadilla <gpdiego370@outlook.com>
2024-04-05 00:56:05 -0600

    feat(modificarRol): edit role

--- util/privilegios/privilegios.js
+++ util/privilegios/privilegios.js
@@ -1,3 +1,5 @@
+// --- 14. Consulta rol. ---
+
 exports.ver_roles = (request, response, next) => {
     // Inicializa la variable de privilegio
     let puedeVerRoles = false;
@@ -23,6 +25,37 @@ exports.ver_roles = (request, response, next) => {
     else {
         response.redirect('/');
         console.log('No tiene el privilegio de ver roles');
+    }
+
+}
+
+// --- 15. Modifica rol. ---
+
+exports.modifica_rol = (request, response, next) => {
+    // Inicializa la variable de privilegio
+    let puedeModificarRoles = false;
+
+    // Obtiene los privilegios del usuario
+    const { Privilegios } = request.session;
+    console.log("Privilegios: ", Privilegios);
+
+    // Verifica si el usuario tiene el privilegio de modificar roles
+    for (let i = 0; i < Privilegios.length; i++) {
+        if (Privilegios[i].Descripcion === 'Modifica rol.') {
+            puedeModificarRoles = true;
+            break;
+        }
+    }
+
+    // Si el usuario tiene el privilegio de modificar roles, permite continuar con la operacion
+    if (puedeModificarRoles) {
+        next();
+    }
+
+    // Si el usuario no tiene el privilegio de modificar roles, redirige a la pagina de inicio
+    else {
+        response.redirect('/');
+        console.log('No tiene el privilegio de modificar roles');
     }
 
 }
