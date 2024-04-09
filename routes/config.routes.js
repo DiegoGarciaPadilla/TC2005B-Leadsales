1e9717d5d89ec3ec024067749c054aa40d3a8491
AndreaMedinaRico <a01705541@tec.mx>
2024-04-09 16:11:25 -0600

    refactor(auth): add authentication check

--- routes/config.routes.js
+++ routes/config.routes.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
+const isAuth = require('../util/privilegios/is-auth');
+
 const rolController = require('../controllers/rol.controller');
 const privilegios = require('../util/privilegios/privilegios');
 
-router.get('/roles', privilegios.ver_roles, rolController.getRoles);
-router.get('/roles/editarRol/:IDRol', privilegios.modifica_rol, rolController.getEditarRol);
-router.post('/roles/editarRol/:IDRol', privilegios.modifica_rol, rolController.postEditarRol);
+router.get('/roles', isAuth, privilegios.ver_roles, rolController.getRoles);
+router.get('/roles/editarRol/:IDRol', isAuth, privilegios.modifica_rol, rolController.getEditarRol);
+router.post('/roles/editarRol/:IDRol', isAuth, privilegios.modifica_rol, rolController.postEditarRol);
 
 module.exports = router;
