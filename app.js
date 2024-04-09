d096312561c5b45530994d46a982d5223dc6dd0d
Chimali Nava <chimalinava@gmail.com>
2024-04-09 17:58:27 -0600

    feat(history): add history mvc

--- app.js
+++ app.js
@@ -64,12 +64,15 @@ const routes = require("./routes/.routes");
 const usuariosRoutes = require("./routes/usuarios.routes");
 const configRoutes = require("./routes/config.routes");
 const leadsRoutes = require("./routes/leads.routes");
+const reportesRoutes = require("./routes/reportes.routes");
 
 // Usar rutas
 app.use("/users", usuariosRoutes);
 app.use("/config", configRoutes);
 app.use("/directory", leadsRoutes);
+app.use("/history", reportesRoutes);
 app.use("/", routes);
+
 
 // Levantar el servidor
 app.listen(port, () => {
--- controllers/reporte.controller.js
+++ controllers/reporte.controller.js
@@ -2,6 +2,39 @@ const Reporte = require('../model/reporte.model');
 
 /* ========== CU. 24 CONSULTA HISTORIAL | Chimali Nava =============== */
 
-
+exports.getReportes = (request, response, next) => {
+    const { idUsuario } = request.session;
+    console.log(idUsuario);
+    Reporte.fetchRol(idUsuario)
+        .then (([idRolFetched, fieldData]) => {
+            if (idRolFetched === 1 || idRolFetched === 2) {
+                Reporte.fetchAll()
+                    .then(([reportesFetched, fieldData]) => {
+                        response.render('history', {
+                            reportes: reportesFetched,
+                            csrfToken: request.csrfToken(),
+                        });
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            }
+            else {
+                Reporte.fetchReportesByUser(idUsuario)
+                    .then(([reportesFetched, fieldData]) => {
+                        response.render('history', {
+                            reportes: reportesFetched,
+                            csrfToken: request.csrfToken(),
+                        });
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            }
+        })
+        .catch((error) => {
+            console.log("trake khe");
+        });
+};
 /* ========================== FIN CU. 24 ==============================  */
 
