9212883ccf51f29f3af14a8472b6cedbbd20c35b
Chimali Nava <chimalinava@gmail.com>
2024-04-09 12:37:26 -0600

    refactor(directory): fix directory route

--- controllers/leads.controller.js
+++ controllers/leads.controller.js
@@ -13,7 +13,7 @@ exports.getLeads = (request, response, next) => {
     if (!privilegios.includes("Ver todos los leads")) {
         Lead.fetchLeadsByUser(correo)
             .then(([leadsFetched, fieldData]) => {
-                response.render('leads', {
+                response.render('directory', {
                     leads: leadsFetched,    
                 });
             })
@@ -24,7 +24,7 @@ exports.getLeads = (request, response, next) => {
     else {
         Lead.fetchAll()
             .then(([leadsFetched, fieldData]) => {
-                response.render('leads', {
+                response.render('directory', {
                     leads: leadsFetched,
                 });
             })
