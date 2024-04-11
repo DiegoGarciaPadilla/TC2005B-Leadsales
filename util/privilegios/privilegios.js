b4547af057b4b6ea862bac7cc9a3fcec243f0127
Chimali Nava <chimalinava@gmail.com>
2024-04-11 11:47:45 -0600

    refactor(history): check privilege CU24

--- util/privilegios/privilegios.js
+++ util/privilegios/privilegios.js
@@ -59,3 +59,35 @@ exports.modifica_rol = (request, response, next) => {
     }
 
 }
+
+// --- 24. Consulta historial. ---
+
+exports.ver_historial = (request, response, next) => {
+    
+    // Inicializa la variable de privilegio
+    let puedeVerHistorial = false;
+
+    // Obtiene los privilegios del usuario
+    const { Privilegios } = request.session;
+    console.log("Privilegios: ", Privilegios);
+
+    // Verifica si el usuario tiene el privilegio de ver historial
+    for (let i = 0; i < Privilegios.length; i++) {
+        if (Privilegios[i].Descripcion === 'Consulta historial.') {
+            puedeVerHistorial = true;
+            break;
+        }
+    }
+
+    // Si el usuario tiene el privilegio de ver roles, permite continuar con la operacion
+    if (puedeVerHistorial) {
+        next();
+    }
+
+    // Si el usuario no tiene el privilegio de ver roles, redirige a la pagina de inicio
+    else {
+        response.redirect('/');
+        console.log('No tiene el privilegio de modificar roles');
+    }
+
+}
