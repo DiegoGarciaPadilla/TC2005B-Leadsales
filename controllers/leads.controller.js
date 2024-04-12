baaea9040171b80e2700f1261d95e628252aecd7
DiegoGarciaPadilla <gpdiego370@outlook.com>
2024-04-11 23:20:02 -0600

    style(controllers): apply airbnb convention

--- controllers/leads.controller.js
+++ controllers/leads.controller.js
@@ -1,56 +1,54 @@
-const Lead = require('../model/leads.model');
+const Lead = require("../model/leads.model");
 
 /* ========== CU. 25 CONSULTA REPORTE EN HISTORIAL | Diego García =============== */
 
-
 /* ========================== FIN CU. 25 ==============================  */
-
 
 /* ========== CU. 10 CONSULTA DIRECTORIO | Diego Lira - Diego García (Puro Peer Programing) =============== */
 
-exports.getLeads = (request, response, next) => {
-    const { correo = "pednobr@gmail.com", privilegios = ["Ver todos los leads"] } = request.session;
+exports.getLeads = (req, res) => {
+    const {
+        correo = "pednobr@gmail.com",
+        privilegios = ["Ver todos los leads"],
+    } = req.session; // Test user
+
     if (!privilegios.includes("Ver todos los leads")) {
         Lead.fetchLeadsByUser(correo)
-            .then(([leadsFetched, fieldData]) => {
-                response.render('directorio', {
-                    leads: leadsFetched,    
-                }); 
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    }
-    else {
-        Lead.fetchAll()
-            .then(([leadsFetched, fieldData]) => {
-                response.render('directorio', {
+            .then(([leadsFetched]) => {
+                res.render("directorio", {
                     leads: leadsFetched,
-                    csrfToken: request.csrfToken(),
                 });
             })
             .catch((error) => {
                 console.log(error);
             });
-        }
+    } else {
+        Lead.fetchAll()
+            .then(([leadsFetched]) => {
+                res.render("directorio", {
+                    leads: leadsFetched,
+                    csrfToken: req.csrfToken(),
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 };
-
 
 /* ========================== FIN CU. 10 ==============================  */
 
 /* ========== CU. 6 CONSULTA LEAD | Sebas Colin =============== */
 
 exports.getLeadDetails = (req, res) => {
-    console.log('pedo');
-    const leadId = req.params.leadId;
-    Lead.fetchOne(leadId)
-    .then(([testLead, fieldData]) => {
-            return res.status(200).json(testLead[0]);
-    }).catch(() => {
-        console.log('Error fetching lead details:');
-        res.status(500).json({ error: 'Internal server error' });
-    });
+    const { leadId } = req.params;
 
+    Lead.fetchOne(leadId)
+        .then(([testLead]) => res.status(200).json(testLead[0]))
+        .catch(() => {
+            console.log("Error fetching lead details:");
+            res.status(500).json({ error: "Internal server error" });
+        });
 };
 
 // exports.getLeadDetails = async (req, res) => {
@@ -59,17 +57,13 @@ exports.getLeadDetails = (req, res) => {
 // };
 
 exports.getLeadDetails = (req, res) => {
-    console.log('pedo');
-    const leadId = req.params.leadId;
-    console.log('IDLead: ', leadId);
+    const { leadId } = req.params;
     Lead.fetchOne(leadId)
-    .then(([testLead, fieldData]) => {
-            return res.status(200).json(testLead[0]);
-    }).catch(() => {
-        console.log('Error fetching lead details:');
-        res.status(500).json({ error: 'Internal server error' });
-    });
-
+        .then(([testLead]) => res.status(200).json(testLead[0]))
+        .catch(() => {
+            console.log("Error fetching lead details:");
+            res.status(500).json({ error: "Internal server error" });
+        });
 };
 
 // exports.getLeadDetails = async (req, res) => {
@@ -77,28 +71,21 @@ exports.getLeadDetails = (req, res) => {
 //     res.send('Hello from getLeadDetails');
 // };
 
-exports.getOneLead = (req, res, next) => {
-
-};
-
 /* ========================== FIN CU. 6 ==============================  */
-
 
 /* ========== CU. 5 CREA LEAD | Diego Lira =============== */
 
-exports.postCrearLead = (request, response) => {
-    const { privilegios = ["Crea lead"]} = request.session;
-    const { nombre, telefono, embudo, asignadoa} = request.body;
-    if(privilegios.includes("Crea lead")){
+exports.postCrearLead = (req, res) => {
+    const { privilegios = ["Crea lead"] } = req.session;
+    const { nombre, telefono, embudo, asignadoa } = req.body;
+    if (privilegios.includes("Crea lead")) {
         console.log(nombre);
         console.log(telefono);
         console.log(embudo);
         console.log(asignadoa);
-        
-    }else{
-        response.redirect("/directorio");
+    } else {
+        res.redirect("/directorio");
     }
 };
 
 /* ========================== FIN CU. 5 ==============================  */
-
--- controllers/reporte.controller.js
+++ controllers/reporte.controller.js
@@ -2,19 +2,19 @@ const Reporte = require('../model/reporte.model');
 
 /* ========== CU. 24 CONSULTA HISTORIAL | Chimali Nava =============== */
 
-exports.getReportes = (request, response, next) => {
-    const { IDUsuario } = request.session;
+exports.getReportes = (req, res) => {
+    const { IDUsuario } = req.session;
     console.log(IDUsuario);
     Reporte.fetchRol(IDUsuario)
-        .then (([idRolFetched, fieldData]) => {
+        .then (([idRolFetched]) => {
             console.log(idRolFetched);
             const rol = idRolFetched[0];
             if (rol.IDRol === 1 || rol.IDRol === 2) {
                 Reporte.fetchAll()
-                    .then(([reportesFetched, fieldData]) => {
-                        response.render('historial', {
+                    .then(([reportesFetched]) => {
+                        res.render('historial', {
                             reportes: reportesFetched,
-                            csrfToken: request.csrfToken(),
+                            csrfToken: req.csrfToken(),
                         });
                     })
                     .catch((error) => {
@@ -23,10 +23,10 @@ exports.getReportes = (request, response, next) => {
             }
             else {
                 Reporte.fetchReportesByUser(IDUsuario)
-                    .then(([reportesFetched, fieldData]) => {
-                        response.render('historial', {
+                    .then(([reportesFetched]) => {
+                        res.render('historial', {
                             reportes: reportesFetched,
-                            csrfToken: request.csrfToken(),
+                            csrfToken: req.csrfToken(),
                         });
                     })
                     .catch((error) => {
@@ -35,7 +35,7 @@ exports.getReportes = (request, response, next) => {
             }
         })
         .catch((error) => {
-            console.log("error");
+            console.log(error);
         });
 };
 /* ========================== FIN CU. 24 ==============================  */
