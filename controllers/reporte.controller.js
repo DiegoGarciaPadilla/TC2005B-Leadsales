3920756b049d1d448e71fb5eedf587da93ace11e
DiegoGarciaPadilla <gpdiego370@outlook.com>
2024-04-17 16:52:23 -0600

    fix(reporte): resolve conflicts

--- controllers/reporte.controller.js
+++ controllers/reporte.controller.js
@@ -1,39 +1,36 @@
-const Chart = require('chart.js');
+const Chart = require("chart.js");
 
-const Reporte = require('../model/reporte.model');
+const Reporte = require("../model/reporte.model");
 
-const Lead = require('../model/leads.model');
+const Lead = require("../model/leads.model");
 
 // const jsPDF = require('jspdf');
 
 /* ========== CU. 02 CONSULTA HISTORIAL | Sebas Colín - Andrea Medina =============== */
 
-exports.postReporte = (request, response, next) => {
-<<<<<<< Updated upstream
-    const { IDUsuario, Correo, Privilegios, Nombre, ApellidoPaterno } = request.session;
-    const NombreCompleto = Nombre + " " + ApellidoPaterno;
-    let valorInput = request.body._csrf;
-    //console.log(NombreCompleto, Privilegios);
-=======
->>>>>>> Stashed changes
-
+exports.postReporte = (req, res, next) => {
     // Obtiene los datos de la sesión
-    const { Correo, Privilegios, Nombre, ApellidoPaterno } = request.session;
+    const { IDUsuario, Correo, Privilegios, Nombre, ApellidoPaterno } =
+        req.session;
+
+    // Obtiene el valor del token CSRF
+    const CSRF = req.body._csrf;
 
     // Obtiene el nombre completo del usuario
     const NombreCompleto = String(Nombre, " ", ApellidoPaterno);
 
     // Recibe las fechas de inicio y fin para la consulta
-    const { start, end } = request.body;
-    
+    const { start, end } = req.body;
+
     console.log(start, end);
 
-    // Caso cuando se pueden consultar los leads de todos  
-     
+    // Caso cuando se pueden consultar los leads de todos
+
     if (
         Privilegios.some(
-        (priv) => priv.Descripcion === "Consulta directorio todos."
-    )) {
+            (priv) => priv.Descripcion === "Consulta directorio todos."
+        )
+    ) {
         // Arreglo de promesas a ser resueltas
         const graphPromises = [];
 
@@ -42,7 +39,7 @@ exports.postReporte = (request, response, next) => {
         const promesaGraphOne = Lead.graphOne(g1);
 
         graphPromises.push(promesaGraphOne);
-        
+
         // Gráfica 2
         const g2 = Lead.fetchAllForGraphs(start, end);
         const promesaGraphTwo = Lead.graphTwo(g2);
@@ -66,7 +63,7 @@ exports.postReporte = (request, response, next) => {
         const promesaGraphFive = Lead.graphFive(g5);
 
         graphPromises.push(promesaGraphFive);
-        
+
         // Gráfica 6
         const g6 = Lead.fetchAllForGraphs(start, end);
         const promesaGraphSix = Lead.graphSix(g6);
@@ -78,7 +75,7 @@ exports.postReporte = (request, response, next) => {
         const promesaGraphSeven = Lead.graphSeven(g7);
 
         graphPromises.push(promesaGraphSeven);
-        
+
         // Gráfica 8
         const g8 = Lead.fetchAllForGraphs(start, end);
         const promesaGraphEight = Lead.graphEight(g8);
@@ -88,39 +85,26 @@ exports.postReporte = (request, response, next) => {
         // Datos de todas las gráficas pendientes a resolver
         Promise.all(graphPromises)
             .then((results) => {
-                const responseData = {
+                const resData = {
                     // Datos a JSON de cada gráfica
-                    graph1Data: results[0][0], 
+                    graph1Data: results[0][0],
                     graph2Data: results[1][0],
                     graph3Data: results[2][0],
                     graph4Data: results[3][0],
-                    graph5Data: results[4][0],     // Datos gráficas
+                    graph5Data: results[4][0], // Datos gráficas
                     graph6Data: results[5][0],
                     graph7Data: results[6][0],
                     graph8Data: results[7][0],
                 };
-<<<<<<< Updated upstream
 
-                // Respuesta de JSON con datos para todas las gráficas
-                //response.status(200).json(responseData);
-                // console.log(responseData);
-                response.render('reporte', { 
-                    data: responseData,
-                    csrfToken: valorInput,
-                 });
-=======
-                
-                // Renderiza la vista de reporte
-                response.render('reporte', { 
-                    csrfToken: request.csrfToken(),
-                    correo: Correo,
-                    data: responseData,
+                res.render("reporte", {
+                    data: resData,
+                    csrfToken: CSRF,
                 });
->>>>>>> Stashed changes
             })
             .catch((error) => {
                 console.log("Error recogiendo datos de leads:", error);
-                response.status(500).json({ error: "Internal server error" });
+                res.status(500).json({ error: "Internal server error" });
             });
     } else {
         // Arreglo de promesas a ser resueltas
@@ -132,7 +116,7 @@ exports.postReporte = (request, response, next) => {
         const promesaGraphOne = Lead.graphOne(g1);
 
         graphPromises.push(promesaGraphOne);
-        
+
         // Gráfica 2
         const g2 = Lead.fetchSomeForGraphs(NombreCompleto, start, end);
         const promesaGraphTwo = Lead.graphTwo(g2);
@@ -162,7 +146,7 @@ exports.postReporte = (request, response, next) => {
         const promesaGraphSix = Lead.graphSix(g6);
 
         graphPromises.push(promesaGraphSix);
-        
+
         // Gráfica 7
         const g7 = Lead.fetchSomeForGraphs(start, end);
         const promesaGraphSeven = Lead.graphSeven(g7);
@@ -178,11 +162,11 @@ exports.postReporte = (request, response, next) => {
         // Datos de todas las gráficas pendientes a resolver
         Promise.all(graphPromises)
             .then((results) => {
-                const responseData = {
+                const resData = {
                     // Datos a JSON de cada gráfica
-                    graph1Data: results[0][0], 
+                    graph1Data: results[0][0],
                     graph2Data: results[1][0],
-                    graph3Data: results[2][0],     // Datos gráficas
+                    graph3Data: results[2][0], // Datos gráficas
                     graph4Data: results[3][0],
                     graph5Data: results[4][0],
                     graph6Data: results[5][0],
@@ -190,30 +174,20 @@ exports.postReporte = (request, response, next) => {
                     graph8Data: results[7][0],
                 };
 
-<<<<<<< Updated upstream
                 // Respuesta de JSON con datos para todas las gráficas
-                //response.status(200).json(responseData);
-                response.render('reporte', { data: responseData, csrfToken: CSRF });
-=======
-                response.render('reporte', { 
-                    csrfToken: request.csrfToken(),
-                    correo: Correo,
-                    data: responseData
+                res.render("reporte", {
+                    data: resData,
+                    csrfToken: CSRF,
                 });
-
->>>>>>> Stashed changes
             })
             .catch((error) => {
                 console.log("Error recogiendo datos de leads:", error);
-                response.status(500).json({ error: "Internal server error" });
+                res.status(500).json({ error: "Internal server error" });
             });
     }
-        
-
-}
+};
 
 /* ========================== FIN CU. 24 ==============================  */
-
 
 /* ========== CU. 24 CONSULTA HISTORIAL | Chimali Nava =============== */
 
@@ -249,7 +223,7 @@ exports.getReportes = (req, res) => {
             .then(([reportesFetched]) => {
                 res.render("historial", {
                     reportes: reportesFetched,
-                    csrfToken: req.csrfToken(),
+                    csrfToken: CSRF,
                 });
             })
             .catch((error) => {
