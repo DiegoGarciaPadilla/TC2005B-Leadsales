30bf2ea5c27639ef727c69b86abf1704136d5165
SebastianCo1126 <colin.sebas02@gmail.com>
2024-04-25 11:04:38 -0600

    fix(errorMessage): mensaje de error al cargar un .csv con mal formato

--- controllers/CSV.controller.js
+++ controllers/CSV.controller.js
@@ -12,29 +12,57 @@ exports.post_CSV = (req, res) => {
     console.log(req.file);
     const csv = new CSV(req.file.filename);
 
-    // Parse CSV and store in database
-    csv.save();
-
-    Usuario.fetchAllUsers()
-        .then(([usuariosFetched]) => {
-            res.render("inicio", {
-                success: "Los leads se han registrado exitosamente.",
-                file: `/uploads/${req.file.filename}`,
-                csrfToken: req.csrfToken(),
-                correo: req.session.Correo,
-                nombre: req.session.Nombre,
-                apellidoPaterno: req.session.ApellidoPaterno,
-                apellidoMaterno: req.session.ApellidoMaterno,
-                rol: req.session.Rol,   
-                usuarios: usuariosFetched,
-                error: "",
-            });
+    csv.check()
+        .then((isValid) => {
+            if (isValid) {
+                csv.save();
+                Usuario.fetchAllUsers()
+                .then(([usuariosFetched]) => {
+                    res.render("inicio", {
+                        success: "Los leads se han registrado exitosamente.",
+                        file: `/uploads/${req.file.filename}`,
+                        csrfToken: req.csrfToken(),
+                        correo: req.session.Correo,
+                        privilegios: req.session.Privilegios,
+                        nombre: req.session.Nombre,
+                        apellidoPaterno: req.session.ApellidoPaterno,
+                        apellidoMaterno: req.session.ApellidoMaterno,
+                        rol: req.session.Rol,   
+                        usuarios: usuariosFetched,
+                        error: "",
+                    });
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+            } else {
+                Usuario.fetchAllUsers()
+                    .then(([usuariosFetched]) => {
+                        res.render("inicio", {
+                            success: "",
+                            file: `/uploads/${req.file.filename}`,
+                            csrfToken: req.csrfToken(),
+                            correo: req.session.Correo,
+                            privilegios: req.session.Privilegios,
+                            nombre: req.session.Nombre,
+                            apellidoPaterno: req.session.ApellidoPaterno,
+                            apellidoMaterno: req.session.ApellidoMaterno,
+                            rol: req.session.Rol,   
+                            usuarios: usuariosFetched,
+                            error: "Tu archivo.csv no tiene el formato correcto!",
+                        });
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            }
         })
         .catch((error) => {
             console.log(error);
-        });
+        })
 
     return null; // Add a return statement at the end of the function
+
 };
 
 /* ========================== FIN CU. 26 ==============================  */
